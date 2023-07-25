6da62362e363eec1f06b9a2cd5670eb4237ddf57
Carifio24 <carifio24@gmail.com>
2023-07-25 17:43:32 -0400

    Add middleware requiring a particular key to access server endpoints.

--- src/server.ts
+++ src/server.ts
@@ -40,7 +40,8 @@ import {
 import { CosmicDSSession } from "./models";
 
 import { ParsedQs } from "qs";
-import express, { Request, Response as ExpressResponse } from "express";
+import { config } from "dotenv";
+import express, { Request, Response as ExpressResponse, NextFunction } from "express";
 import { Response } from "express-serve-static-core";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
@@ -49,8 +50,12 @@ import sequelizeStore from "connect-session-sequelize";
 import { v4 } from "uuid";
 import cors from "cors";
 import jwt from "jsonwebtoken";
+
+import { isValidAPIKey } from "./authorization";
 import { isStudentOption } from "./models/student_options";
+
 export const app = express();
+config();
 
 // TODO: Clean up these type definitions
 
@@ -105,6 +110,17 @@ const store = new SequelizeStore({
   }
 });
 
+function apiKeyMiddleware(req: Request, res: ExpressResponse, next: NextFunction): void {
+  const key = req.get("Authorization");
+  if (isValidAPIKey(key)) {
+    next();
+  } else {
+    res.statusCode = 401;
+    res.json({ message: "You must provide a valid CosmicDS API key!" });
+    res.end();
+  }
+}
+
 const SECRET = "ADD_REAL_SECRET";
 const SESSION_NAME = "cosmicds";
 
@@ -124,6 +140,8 @@ app.use(session({
   }
 }));
 store.sync();
+
+app.use(apiKeyMiddleware);
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
