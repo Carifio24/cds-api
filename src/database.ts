bf623bc0dd3f8c8ca714302c39f864fe25264b81
Carifio24 <carifio24@gmail.com>
2023-07-27 15:49:42 -0400

    Add implementation to check if API key is valid.

--- src/database.ts
+++ src/database.ts
@@ -32,6 +32,8 @@ import { setUpAssociations } from "./associations";
 import { initializeModels } from "./models";
 import { StudentOption, StudentOptions } from "./models/student_options";
 import { Question } from "./models/question";
+import { APIKey } from "./models/api_key";
+import { SHA3 } from "sha3";
 
 type SequelizeError = { parent: { code: string } };
 
@@ -63,6 +65,8 @@ export const cosmicdsDB = new Sequelize(dbName, username, password, {
     }
 });
 
+const HASHER = new SHA3(256);
+
 // Initialize our models with our database connection
 initializeModels(cosmicdsDB);
 // (async () => {
@@ -72,6 +76,18 @@ initializeModels(cosmicdsDB);
 
 // Create any associations that we need
 setUpAssociations();
+
+function hashKey(key: string): string {
+  HASHER.reset();
+  HASHER.update(key);
+  return HASHER.digest("hex");
+}
+
+export async function isValidAPIKey(key: string): Promise<boolean> {
+  const hashedKey = hashKey(key);
+  const apiKey = await APIKey.findOne({ where: { hashed_key: hashedKey } });
+  return apiKey !== null;
+}
 
 // For now, this just distinguishes between duplicate account creation and other errors
 // We can flesh this out layer
