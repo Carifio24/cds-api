ec899ce2ce2ed4274a3166a9425f100fb242e29a
Carifio24 <carifio24@gmail.com>
2024-09-28 11:41:17 -0400

    Basic test setup seems to be working now.

--- tests/root.test.ts
+++ tests/root.test.ts
@@ -1,15 +1,45 @@
-import { describe, it } from "@jest/globals";
+import { afterAll, beforeAll, describe, expect, it } from "@jest/globals";
+import type { Express } from "express";
+import type { Sequelize } from "sequelize";
+import request from "supertest";
 
-import testApp, { authorizedRequest, runApp } from "./utils";
+import { authorize, createMySQLConnection, createTestApp, setupTestDatabase } from "./utils";
+import { setupApp } from "../src/app";
+
+let testDB: Sequelize;
+let testApp: Express;
+beforeAll(async () => {
+  testDB = await setupTestDatabase();
+  testApp = createTestApp(testDB);
+  setupApp(testApp, testDB);
+}, 100_000);
+
+afterAll(async () => {
+  const conn = await createMySQLConnection();
+  await conn.query("DROP DATABASE test;");
+});
+
 
 describe("Test root route", () => {
-  runApp(testApp);
+
+  it("Should show a welcome message + warning", async () => {
+    authorize(request(testApp).get("/"))
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .expect({ message: "Welcome to the CosmicDS server! You'll need to include a valid API key with your requests in order to access other endpoints." });
+  });
 
   it("Should show a welcome message", async () => {
-    authorizedRequest(testApp)
-      .get("/")
+    console.log("Starting test!");
+    authorize(request(testApp).get("/"))
       .expect(200)
       .expect("Content-Type", /json/)
       .expect({ message: "Welcome to the CosmicDS server!" });
+
+    console.log("Done!");
+  }, 10_000);
+
+  it("Should run", () => {
+    expect(1 + 1).toBe(2);
   });
 });
--- tests/utils.ts
+++ tests/utils.ts
@@ -1,37 +1,69 @@
-import { Express } from "express";
-import request from "supertest";
+import type { Express } from "express";
+import type { Server } from "http";
+import type { Test } from "supertest";
 import { Sequelize } from "sequelize";
 
 import { setUpAssociations } from "../src/associations";
 import { initializeModels } from "../src/models";
 import { createApp } from "../src/server";
+import { APIKey } from "../src/models/api_key";
+import { config } from "dotenv";
+import { getDatabaseConnection } from "../src/database";
+import { createConnection, Connection } from "mysql2/promise";
 
-export function authorizedRequest(app: Express) {
-  return request(app)
-    .set("Authorization", process.env.CDS_API_KEY);
+export function authorize(request: Test): Test {
+  return request.set({ Authorization: process.env.CDS_API_KEY });
 }
 
-// Just a pass-through for now
-// Maybe we'll add more in the future
-export function unauthorizedRequest(app: Express) {
-  return request(app);
+export async function createMySQLConnection(): Promise<Connection> {
+  return createConnection({
+    host: process.env.DB_HOSTNAME as string,
+    user: process.env.DB_USERNAME as string,
+    password: process.env.DB_PASSWORD as string,
+  });
 }
 
-function createTestDatabase(): Sequelize {
-  const db = new Sequelize({ dialect: "mysql" });
-  db.query("CREATE DATABASE IF NOT EXISTS test");
-
+export async function setupTestDatabase(): Promise<Sequelize> {
+  config();
+  const username = process.env.DB_USERNAME as string;
+  const password = process.env.DB_PASSWORD as string;
+  const connection = await createMySQLConnection();
+  await connection.query("CREATE DATABASE IF NOT EXISTS test;");
+  const db = getDatabaseConnection({
+    dbName: "test",
+    username,
+    password,
+  });
+  await db.query("USE test;");
   initializeModels(db);
   setUpAssociations();
+
+  // We need to close when the connection terminates!
+  // See https://github.com/sequelize/sequelize/issues/7953
+  // and https://stackoverflow.com/a/45114507
+  // db.sync({ force: true, match: /test/ }).finally(() => db.close());
+  await addTestData();
+
   return db;
 }
 
-export function runApp(app: Express, port=8080) {
-  app.listen(port);
+export async function addAPIKey(): Promise<APIKey | void> {
+  // Set up some basic data that we're going to want
+  await APIKey.sync({ force: true });
+  return APIKey.create({
+    hashed_key: process.env.HASHED_API_KEY as string,
+    client: "Tests",
+  });
 }
 
-const testDB = createTestDatabase();
-console.log(testDB);
-const app = createApp(testDB);
+export async function addTestData() {
+  await addAPIKey();
+}
 
-export default app;
+export function createTestApp(db: Sequelize): Express {
+  return createApp(db);
+}
+
+export function runApp(app: Express, port=8080, callback?: () => void): Server {
+  return app.listen(port, callback);
+}
