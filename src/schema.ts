0994b32cff532678d465d42ce8d9ba6a1191a0df
Jon Carifio <carifio24@gmail.com>
2026-08-26 15:17:59 -0400

    WIP: First draft of function for creating effect schema from Sequelize model.

---
+++ src/schema.ts
@@ -0,0 +1,60 @@
+import * as S from "@effect/schema/Schema";
+import { DataTypes, ModelAttributeColumnOptions, ModelStatic, type Model } from "sequelize";
+
+function schemaForAttribute<M extends Model>(attribute: ModelAttributeColumnOptions<M>) {
+  const type: string = typeof attribute.type === "string" ? attribute.type : attribute.type.key;
+
+  const stringTypes = [DataTypes.STRING, DataTypes.TEXT].map(t => t.key);
+  if (stringTypes.includes(type)) {
+    return S.string;
+  }
+
+  const intTypes = [DataTypes.INTEGER, DataTypes.INTEGER.UNSIGNED].map(t => t.key);
+  if (intTypes.includes(type)) {
+    return S.number.pipe(S.int());
+  }
+
+  const bigIntTypes = [DataTypes.BIGINT, DataTypes.BIGINT.UNSIGNED].map(t => t.key);
+  if (bigIntTypes.includes(type)) {
+    return S.bigint;
+  }
+
+  switch (type) {
+    case DataTypes.ENUM.key:
+      const options: Record<string, string | number> = {};
+      attribute.values?.forEach(val => { options[val] = val; });
+      return S.enums(options);
+
+    // TODO: What to do about dates?
+    case DataTypes.DATE.key:
+      return S.string;
+
+    case DataTypes.DATEONLY.key:
+      return S.string;
+
+    case DataTypes.BOOLEAN.key:
+      return S.boolean;
+
+    case DataTypes.JSON.key:
+      return S.object;
+  }
+
+  return null;
+}
+
+export function modelToEffectSchema<M extends Model>(modelType: ModelStatic<M>): S.Schema<any,any,never> {
+  const structOptions: Record<string, S.Schema<any,any,never>> = {};
+  const attributes = modelType.getAttributes();
+  Object.entries(attributes).forEach(([key, attr]) => {
+    let schema: S.Schema<any, any, never> | null = schemaForAttribute(attr);
+    if (!schema) {
+      return;
+    }
+    if (attr.allowNull) {
+      schema = S.nullish(schema);
+    }
+    structOptions[key] = schema;
+  });
+
+  return S.struct(structOptions);
+}
