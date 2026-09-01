308d7695bba906c5f43abcde01df546d8186f59a
Carifio24 <carifio24@gmail.com>
2026-09-01 14:06:44 -0400

    Update why-roman setup function to match new format.

--- src/stories/why-roman/router.ts
+++ src/stories/why-roman/router.ts
@@ -1,11 +1,10 @@
-import type { Sequelize } from "sequelize";
-import type { Express } from "express";
 import { storyRouter } from "../../story_router";
 import { WhyRomanData, initializeModels } from "./models";
 import { getWhyRomanData, submitWhyRomanData, updateWhyRomanData } from "./database";
+import type { StorySetupParams } from "../../types";
 
-export function setup(_app: Express, db: Sequelize) {
-  initializeModels(db);
+export function setup(params: StorySetupParams) {
+  initializeModels(params.db);
 }
 
 export function router() {
